56274b2ae08d6c85c81684a9ca1a495f0b64e51b
TTimotti <besttim1@naver.com>
2023-01-16 17:41:20 +0900

    project_wac

---
+++ wac/src/test/java/com/wac/WacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
