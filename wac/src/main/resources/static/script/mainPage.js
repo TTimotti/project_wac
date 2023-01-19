75cfb50083b6e35eac1e97674e431ee634a18de3
itwill <itwill@DESKTOP-68VI7GK>
2023-01-19 11:26:25 +0900

    메인페이지 이미지 전환 추가 - 김지훈

--- wac/src/main/resources/static/script/mainPage.js
+++ wac/src/main/resources/static/script/mainPage.js
@@ -6,13 +6,28 @@ const bannerImage = document.querySelector('#bannerImg');
 const banner = document.querySelector('.bImg');
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
-nextBtn.addEventListener("click", () => {
-	console.log('bannerImageChange')
-	bannerImage.setAttribute('src', '/img/banner/1.jpg')
+let i = 0;
+nextBtn.addEventListener("click", () => {		
+	if (i == 0) {
+		i = 1;
+	}
+	i += 1;	
+	if (i == 5) {
+		i = 1;	
+	} 
+	bannerImage.setAttribute('src', '/img/banner/' + i + '.jpg');
 });
 
 prevBtn.addEventListener("click", () => {
-	console.log('bannerImageChange')
-	bannerImage.setAttribute('src', '/img/banner/2.jpg')
+	if (i == 0) {
+		i = 5;
+	}
+	i -= 1;
+	if (i == 0) {
+		i = 4;
+	}		
+	bannerImage.setAttribute('src', '/img/banner/' + i + '.jpg');
+	
+
 });    
-    
+   
