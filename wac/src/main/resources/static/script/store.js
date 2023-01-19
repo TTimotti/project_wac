4e1bea1d0c9c53c0a34fb03a3313c19fc167f94d
ITWILL <ITWILL@DESKTOP-A9BJE77>
2023-01-19 16:54:43 +0900

    . 맵 마커 왁도날드 커스텀 + 왁도날드 지점 임시 추가 - 장민석.

--- wac/src/main/resources/static/script/store.js
+++ wac/src/main/resources/static/script/store.js
@@ -8,13 +8,13 @@
  *  카카오 맵 구현.
  */
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
-    mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+	mapOption = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3 // 지도의 확대 레벨
+	};
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
- 
+
 // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
 var mapTypeControl = new kakao.maps.MapTypeControl();
 
@@ -25,12 +25,22 @@ map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
- 
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	mapOption = {
+		center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
+		level: 4 // 지도의 확대 레벨
+	};
+
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
 
+var imageSrc = 'https://cdn.discordapp.com/attachments/685937478435471438/1065524257003683920/wac.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 // 주소로 좌표를 검색합니다
-geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function(result, status) {
+geocoder.addressSearch('서울 강남구 테헤란로 124 삼원타워', function(result, status) {
 
     // 정상적으로 검색이 완료됐으면 
      if (status === kakao.maps.services.Status.OK) {
@@ -40,66 +50,80 @@ geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function
         // 결과값으로 받은 위치를 마커로 표시합니다
         var marker = new kakao.maps.Marker({
             map: map,
-            position: coords
+            position: coords,
+            image: markerImage 
         });
 
         // 인포윈도우로 장소에 대한 설명을 표시합니다
         var infowindow = new kakao.maps.InfoWindow({
-            content: 
-            '<div style="width:250px;text-align:center;padding:6px 0;">왁도날드 강남 1호점</div>'
+            content: '<div style="width:190px;text-align:center;padding:6px 0;">왁도날드 강남 삼원타워점</div>'
         });
         infowindow.open(map, marker);
 
         // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
         map.setCenter(coords);
     } 
-});    
+});   
 
-var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+geocoder.addressSearch('서울 강남구 강남대로 지하 396', function(result, status) {
 
-// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-    markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+    // 정상적으로 검색이 완료됐으면 
+     if (status === kakao.maps.services.Status.OK) {
 
-// 마커를 생성합니다
-var marker = new kakao.maps.Marker({
-  position: markerPosition,
-  image: markerImage // 마커이미지 설정 
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords,
+            image: markerImage 
+        });
+
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+            content: '<div style="width:190px;text-align:center;padding:6px 0;">왁도날드 강남역점</div>'
+        });
+        infowindow.open(map, marker);
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    } 
 });
 
-// 마커가 지도 위에 표시되도록 설정합니다
-marker.setMap(map);  
+geocoder.addressSearch('서울 강남구 강남대로 438 스타플렉스', function(result, status) {
 
-// 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-var content = '<div class="customoverlay">' +
-    '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
-    '    <span class="title">구의야구공원</span>' +
-    '  </a>' +
-    '</div>';
+    // 정상적으로 검색이 완료됐으면 
+     if (status === kakao.maps.services.Status.OK) {
 
-// 커스텀 오버레이가 표시될 위치입니다 
-var position = new kakao.maps.LatLng(37.54699, 127.09598);  
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-// 커스텀 오버레이를 생성합니다
-var customOverlay = new kakao.maps.CustomOverlay({
-    map: map,
-    position: position,
-    content: content,
-    yAnchor: 1 
-});
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords,
+            image: markerImage 
+        });
 
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+            content: '<div style="width:190px;text-align:center;padding:6px 0;">왁도날드 강남 CGV점</div>'
+        });
+        infowindow.open(map, marker);
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    } 
+});      
 // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
 function makeOverListener(map, marker, infowindow) {
-    return function() {
-        infowindow.open(map, marker);
-    };
+	return function() {
+		infowindow.open(map, marker);
+	};
 }
 
 // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
 function makeOutListener(infowindow) {
-    return function() {
-        infowindow.close();
-    };
+	return function() {
+		infowindow.close();
+	};
 }
