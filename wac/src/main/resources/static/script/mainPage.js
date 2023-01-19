a2550be8fa218c62b9251b0407cb6858502bfe4e
itwill <itwill@DESKTOP-68VI7GK>
2023-01-19 10:21:42 +0900

    header 수정, img 추가

---
+++ wac/src/main/resources/static/script/mainPage.js
@@ -0,0 +1,9 @@
+/*
+header.js / script
+*/
+
+const bannerImage = document.querySelector('#bannerImg');
+const banner = document.querySelector('#bImg');
+banner.addEventListener('DOMContentLoaded', click => {
+	console.log(bannerImage, banner)
+    });
