afb671db8b66a89dc5a14fccdf4924b2d64013ad
itwill <itwill@DESKTOP-68VI7GK>
2023-01-19 11:08:43 +0900

    배너 이미지 버튼 기능 추가 - 김지훈

--- wac/src/main/resources/static/script/mainPage.js
+++ wac/src/main/resources/static/script/mainPage.js
@@ -3,7 +3,16 @@ header.js / script
 */
 
 const bannerImage = document.querySelector('#bannerImg');
-const banner = document.querySelector('#bImg');
-banner.addEventListener('DOMContentLoaded', click => {
-	console.log(bannerImage, banner)
-    });
+const banner = document.querySelector('.bImg');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+nextBtn.addEventListener("click", () => {
+	console.log('bannerImageChange')
+	bannerImage.setAttribute('src', '/img/banner/1.jpg')
+});
+
+prevBtn.addEventListener("click", () => {
+	console.log('bannerImageChange')
+	bannerImage.setAttribute('src', '/img/banner/2.jpg')
+});    
+    
