add50db7c55937df3ccd53bfcfab6bb8545df680
itwill <itwill@DESKTOP-7APGKI7>
2023-01-30 13:51:38 +0900

    .

--- wac/src/main/java/com/wac/controller/MenuController.java
+++ wac/src/main/java/com/wac/controller/MenuController.java
@@ -1,15 +1,17 @@
 package com.wac.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.wac.domain.Menu;
 import com.wac.dto.MenuCreateDto;
@@ -60,13 +62,13 @@ public class MenuController {
      * @author 추지훈
      */
     @PostMapping("/menu/create")
-    public String create(MenuCreateDto dto) { // ,  @RequestParam("image") MultipartFile file) throws IllegalStateException, IOException {
+    public String create(MenuCreateDto dto,  @RequestParam("image") MultipartFile file) throws IllegalStateException, IOException {
         log.info("create() dto = {}", dto);
         // Menu entity = 
         menuService.create(dto);
         
-//        Integer img = imagesService.saveMenuImage(file);
-//        dto.setImage(img);
+        Integer img = imagesService.saveMenuImage(file);
+        dto.setImage(img);
         
         return "redirect:/menu";
     }
