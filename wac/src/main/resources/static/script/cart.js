aa765a7fc27f457168bb9ab5e69a06dc77218e3b
leejongyu <helloy22@naver.com>
2023-01-31 18:10:55 +0900

    css camel로 통일

--- wac/src/main/resources/static/script/cart.js
+++ wac/src/main/resources/static/script/cart.js
@@ -3,23 +3,41 @@
  */
 
 
-    
+
 window.addEventListener('DOMContentLoaded', function() {
 
     // 로그인 유저 체크
     const ifDifferent = document.querySelector('#ifDifferent');
     const ifSame = document.querySelector('#ifSame');
-    
+
     console.log(loginUser);
     console.log(userName);
-    
-        if(userName == loginUser) {
-            ifSame.style.display="block";
-            
-        } else{
-            ifDifferent.style.display="block";
-        }
-    
-    // 버튼 기능 활성화
 
+    if (userName == loginUser) {
+        ifSame.style.display = "block";
+
+    } else {
+        ifDifferent.style.display = "block";
+    }
+
+    // 버튼 기능 활성화
+    const cartDiv = document.querySelector('#cartDiv');
+    const orderDiv = document.querySelector('#orderDiv');
+    const btnOrder = document.querySelector('#btnOreder');
+    const btnCart = document.querySelector('#btnCart');
+
+    // Order 버튼 누르면 Order Dive 활성화
+    btnOrder.addEventListener('click', function() {
+
+    });
+
+    // Cart 버튼 누르면 Cart div 활성화
+    btnCart.addEventListener('click', function() {
+
+    });
+
+    function OrderDiv() {
+    
+    }
+    
 });
