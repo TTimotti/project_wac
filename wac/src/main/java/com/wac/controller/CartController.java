ca393a8a95a93f5f72181cad53acf2a46a89b4d1
leejongyu <helloy22@naver.com>
2023-01-31 17:11:56 +0900

    cart 페이지 열심히 만드는중 - 이존규

---
+++ wac/src/main/java/com/wac/controller/CartController.java
@@ -0,0 +1,65 @@
+package com.wac.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wac.domain.Cart;
+import com.wac.domain.Order;
+import com.wac.domain.Users;
+import com.wac.service.CartService;
+import com.wac.service.OrderService;
+import com.wac.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Cart를 관리하기 위한 controller
+ * 
+ * @author 이존규
+ */
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class CartController {
+
+	private final UserService userService;
+	private final OrderService orderService;
+	private final CartService cartService;
+	
+	@GetMapping("cart")
+	public void cartPage(Model model, String userName, Integer userId) {
+		log.info("cart page = userId{}, userName{]", userId, userName);
+		  Users user = null;
+		  Cart cart = null;
+		  Order order = null;
+		  
+	        if(userName == null) {
+	            user = userService.read(userId);
+	        } else {
+	            user = userService.read(userName);
+	        }
+	    
+	        cart = cartService.read(user.getUserId());
+	        order = orderService.read(user.getUserId());
+	        
+	        if(cart == null) {
+	        	
+	        } else {
+		        model.addAttribute("cart", cart);
+	        }
+	        
+	        if(order == null) {
+
+	        }else {
+		        model.addAttribute("order",order);
+	        }
+	        model.addAttribute("user", user);
+	        
+	}
+	
+	
+}
--- wac/src/main/java/com/wac/service/OrderService.java
+++ wac/src/main/java/com/wac/service/OrderService.java
@@ -1,13 +1,42 @@
 package com.wac.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wac.domain.Order;
+import com.wac.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
+/**
+ * Order Service
+ * @author 이존규
+ *
+ */
 @Slf4j
 @Service 
 @RequiredArgsConstructor
 public class OrderService {
 
+	private final OrderRepository orderRepository;
+	
+	/**
+	 * 모든 order 출력
+	 * @return
+	 */
+    @Transactional(readOnly = true)
+    public List<Order> read() {
+    	log.info("read all");
+    	
+    	return orderRepository.findByOrderByUserIdDesc();
+    }
+	
+    @Transactional(readOnly = true)
+    public Order read(Integer userId) {
+    	log.info("read(userId)");
+    	
+    	return orderRepository.findByUserId(userId).orElse(null);
+    }
 }
