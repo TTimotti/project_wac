24c61febbef47445a8a6a0bacf46528ef49907a5
itwill <itwill@DESKTOP-68VI7GK>
2023-02-01 16:08:14 +0900

    .

--- wac/src/main/resources/static/script/header.js
+++ wac/src/main/resources/static/script/header.js
@@ -7,7 +7,7 @@ function navigo (){
 document.addEventListener('scroll', onScroll, { passive: true });//스크롤 이벤트
  function onScroll () {
      const scrollposition = pageYOffset;//스크롤 위치
-   const nav = document.querySelector('.nav');//네비게이션
+   const nav = document.querySelector('logo');//네비게이션
    if (headerheight<=scrollposition){//만약 헤더높이<=스크롤위치라면
      nav.classList.add('fix')//fix클래스를 네비에 추가
    }
