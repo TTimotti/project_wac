402d2cb1f1dfd996ed488af4d3c480a77357c709
itwill <itwill@DESKTOP-68VI7GK>
2023-02-01 10:59:45 +0900

    summernote 추가
    post\create.html 에 summernote editor 추가 - 김지훈

--- wac/src/main/resources/static/script/postCreate.js
+++ wac/src/main/resources/static/script/postCreate.js
@@ -1,44 +1,39 @@
 /**
- * 
+ * postCreate.js - 김지훈
  */
- 
+$(document).ready(function() {
+  $('#summernote').summernote({
+        tabsize: 2,
+        height: 400
+      });
+});
+
  window.addEventListener('DOMContentLoaded', function() {
    
    const titleInput = document.querySelector('#title');
    const kindInput = document.querySelector('#kind');
    const authorInput = document.querySelector('#author');
    const contentInput = document.querySelector('#content');
-   const imageInput = document.querySelector('#image');
    const btnCreatePost = document.querySelector('#btnSubmit');
    const formCreatePost = document.querySelector('#formCreatePost');
    
    btnCreatePost.addEventListener('click', function() {
-    const title = titleInput.value;
-    const kind = kindInput.value;
-    const author = authorInput.value;
-    const content = contentInput.value;
-    const image = imageInput.value;
+     const title = titleInput.value;
+     const kind = kindInput.value;
+     const author = authorInput.value;
+     const content = contentInput.value;
     
-    if (title.length == 0 || kind.length == 0 || author.length == 0 || content.length == 0) {
-        alert("모두 작성해주세요.");
-    } else {
-        const result = confirm('생성하시겠습니까');
-        if (result) {
-            formCreatePost.action = '/post/create';
-            formCreatePost.method = 'post';
-            formCreatePost.submit();
+     if (title.length == 0 || kind.length == 0 || author.length == 0 || content.length == 0) {
+         alert("모두 작성해주세요.");
+     } else {
+         const result = confirm('생성하시겠습니까');
+         if (result) {
+             formCreatePost.action = '/post/create';
+             formCreatePost.method = 'post';
+             formCreatePost.submit();
             
-            alert("추가됨.")
-        }
-    }
-    
-    
-    
-    
-})
-   
-   
-   
-   
-    
+             alert("추가됨.")
+         }
+     }    
+  })    
 });
