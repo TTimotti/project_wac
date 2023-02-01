70759458521530ef2ee4bed762235d8b1a73b3c0
itwill <itwill@DESKTOP-7APGKI7>
2023-02-01 16:04:51 +0900

    오더 진행중 - 추지훈

--- wac/src/main/java/com/wac/controller/CartController.java
+++ wac/src/main/java/com/wac/controller/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wac.domain.Cart;
 import com.wac.domain.Menu;
