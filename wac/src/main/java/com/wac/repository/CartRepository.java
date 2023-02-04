37d5d21a5d6e801f56e06f13f147b63dea778988
leejongyu <helloy22@naver.com>
2023-02-04 18:23:49 +0900

    cart - findByUserId 추가 - 이존규

--- wac/src/main/java/com/wac/repository/CartRepository.java
+++ wac/src/main/java/com/wac/repository/CartRepository.java
@@ -1,6 +1,7 @@
 package com.wac.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,12 +9,14 @@ import com.wac.domain.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
-	
 	/**
 	 * user Id를 이용해서 Cart 내역 검색
+	 * 
 	 * @param userId
 	 * @return
 	 */
 	List<Cart> findByUserIdOrderByCartId(Integer userId);
-	
+
+	Optional<Cart> findByUserId(Integer userId);
+
 }
