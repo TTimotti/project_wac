83a8201145ccc56e8fcc2996b480e26cb2d11050
itwill <itwill@DESKTOP-68VI7GK>
2023-02-06 10:25:31 +0900

    img size 변경 - 김지훈

--- wac/src/main/resources/static/script/menu.js
+++ wac/src/main/resources/static/script/menu.js
@@ -74,7 +74,7 @@ function updateMenuList(data) {
         + m.menuId /* 여기 링크에 상세페이지 달기 */
         + '">'
         + '<img src="/image/display?fid=' + m.image 
-        + '" alt="메뉴 이미지" style="width:350px; height:200px;"/>'
+        + '" alt="메뉴 이미지" style="width:250px; height:200px;"/>'
         + '<div><strong class="ko">' + m.menuName
         + '</strong><p class="en">' + m.content + '</p>'
         + '</div></a>'
