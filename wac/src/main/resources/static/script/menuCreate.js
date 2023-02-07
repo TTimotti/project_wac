7aa2e18526c49fa825360db9544731eb2d9fb65c
ITWILL <ITWILL@DESKTOP-ITMLQU7>
2023-02-07 18:21:52 +0900

    메뉴 생성 페이지 수정(서범수)

--- wac/src/main/resources/static/script/menuCreate.js
+++ wac/src/main/resources/static/script/menuCreate.js
@@ -33,7 +33,31 @@
     }
       
 })
-   
+
+let img = document.querySelector('input#image'); 
+         console.log(img);
+         
+         function readImage(input) {
+                // 인풋 태그에 파일이 있는 경우
+                if(input.files && input.files[0]) {
+                    // 이미지 파일인지 검사 (생략)
+                    // FileReader 인스턴스 생성
+                    const reader = new FileReader();
+                    // 이미지가 로드가 된 경우
+                    reader.onload = e => {
+                        const previewImage = document.querySelector('img#menuImage');
+                        previewImage.setAttribute('src', e.target.result);
+                    }
+                    // reader가 이미지 읽도록 하기
+                    reader.readAsDataURL(input.files[0]);
+                }
+            }
+            // input file에 change 이벤트 부여
+
+            img.addEventListener("change", e => {
+                console.log('change');
+                readImage(e.target);
+            })
    
    
    
